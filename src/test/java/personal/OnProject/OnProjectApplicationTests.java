5a6f442cd5e555600c369cc73f05a7d8a731da03
kkj000 <kkjeven@gmail.com>
2024-12-19 09:27:11 +0900

    c

---
+++ src/test/java/personal/OnProject/OnProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.OnProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
